7654e91c97aa4aab9de8cb7e8e6ba243c6da2e3a
@arnaudfontaine.web <arnaudfontaine.web@gmail.com>
2021-11-14 22:00:27 +0100

    final app

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import requests
-from flask import Flask
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     response = requests.get('https://ghibliapi.herokuapp.com/films/').content
-    return response
+    films = json.loads(response)
+    return render_template('index.html', films=films)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
